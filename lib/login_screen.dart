af99f8b80d54c41ee328ce8623ce686e1bdca1f1
Mostefaoui-Mohammed-Amin <mohammed.amin.mostefaoui@gmail.com>
2020-09-13 19:34:39 +0100

    adding animation when keyboard show up

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -6,89 +6,154 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
   bool _obscureText = true;
   IconData _iconData = Icons.visibility;
+  AnimationController _controller;
+  Animation _animation;
+  FocusNode _focusEmail = FocusNode();
+  FocusNode _focusPassword = FocusNode();
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animation = Tween<double>(begin: 350, end: 50).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _focusEmail.addListener(() {
+      if (_focusEmail.hasFocus) {
+        print('pass has focus');
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+    _focusPassword.addListener(() {
+      if (_focusPassword.hasFocus) {
+        print('email has focus');
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusEmail.dispose();
+    _focusPassword.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 100),
-      margin: EdgeInsets.only(left: 20, right: 20),
-      child: Form(
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
         child: Column(
           children: [
-            Text('Welcome Back to'),
-            RichText(
-              text: TextSpan(children: [
-                TextSpan(
-                    text: 'Boo', style: TextStyle(color: Colors.redAccent)),
-                TextSpan(
-                    text: 'Stock', style: TextStyle(color: Colors.yellowAccent))
-              ]),
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.fiber_manual_record,
-                    color: Colors.white,
-                  ),
-                  hintText: 'email@address.com',
-                  hintStyle: TextStyle(color: Colors.white),
-                  fillColor: Colors.black,
-                  filled: true),
-            ),
-            TextFormField(
-              obscureText: _obscureText,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.vpn_key,
-                    color: Colors.white,
-                  ),
-                  suffix: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                          if (_obscureText) {
-                            _iconData = Icons.visibility;
-                          } else {
-                            _iconData = Icons.visibility_off;
-                          }
-                        });
-                      },
-                      child: Icon(
-                        _iconData,
-                        color: Colors.white,
-                      )),
-                  hintText: '.  .  .  .  .  .  .  .  .',
-                  hintStyle: TextStyle(color: Colors.white),
-                  fillColor: Colors.black,
-                  filled: true),
+            SizedBox(
+              height: _animation.value,
             ),
             Container(
-              width: double.infinity,
-              child: RaisedButton(
-                color: Colors.redAccent,
-                child: Text(
-                  'SIGN IN',
-                  style: TextStyle(color: Colors.white),
+              margin: EdgeInsets.only(
+                left: 20,
+                right: 20,
+              ),
+              child: Form(
+                child: Column(
+                  children: [
+                    Text('Welcome Back to'),
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: 'Boo',
+                            style: TextStyle(color: Colors.redAccent)),
+                        TextSpan(
+                            text: 'Stock',
+                            style: TextStyle(color: Colors.yellowAccent))
+                      ]),
+                    ),
+                    TextFormField(
+                      focusNode: _focusEmail,
+                      keyboardType: TextInputType.emailAddress,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.fiber_manual_record,
+                            color: Colors.white,
+                          ),
+                          hintText: 'email@address.com',
+                          hintStyle: TextStyle(color: Colors.white),
+                          fillColor: Colors.black,
+                          filled: true),
+                    ),
+                    TextFormField(
+                      focusNode: _focusPassword,
+                      obscureText: _obscureText,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.vpn_key,
+                            color: Colors.white,
+                          ),
+                          suffix: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                  if (_obscureText) {
+                                    _iconData = Icons.visibility;
+                                  } else {
+                                    _iconData = Icons.visibility_off;
+                                  }
+                                });
+                              },
+                              child: Icon(
+                                _iconData,
+                                color: Colors.white,
+                              )),
+                          hintText: '.  .  .  .  .  .  .  .  .',
+                          hintStyle: TextStyle(color: Colors.white),
+                          fillColor: Colors.black,
+                          filled: true),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        color: Colors.redAccent,
+                        child: Text(
+                          'SIGN IN',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: "Don't have an account?",
+                            style: TextStyle(color: Colors.redAccent)),
+                        TextSpan(
+                            text: 'Create one',
+                            style: TextStyle(color: Colors.yellowAccent)),
+                        TextSpan(
+                            text: '.',
+                            style: TextStyle(color: Colors.redAccent))
+                      ]),
+                    ),
+                  ],
                 ),
-                onPressed: () {},
               ),
             ),
-            RichText(
-              text: TextSpan(children: [
-                TextSpan(
-                    text: "Don't have an account?",
-                    style: TextStyle(color: Colors.redAccent)),
-                TextSpan(
-                    text: 'Create one',
-                    style: TextStyle(color: Colors.yellowAccent)),
-                TextSpan(text: '.', style: TextStyle(color: Colors.redAccent))
-              ]),
-            )
           ],
         ),
       ),
